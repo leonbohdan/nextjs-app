fd822fb45cef7145fd78d541d800cbeeeb6eed6b
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-10-14 23:58:24 +0300

    3 pages were added

---
+++ pages/address.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Address() {
+  return (
+    <>
+      <h1>Address</h1>
+
+      <button>
+        <Link href="/">Back Home</Link>
+      </button>
+    </>
+  );
+}
---
+++ pages/summary.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Summary() {
+  return (
+    <>
+      <h1>Summary</h1>
+
+      <button>
+        <Link href="/">Back Home</Link>
+      </button>
+    </>
+  );
+}
